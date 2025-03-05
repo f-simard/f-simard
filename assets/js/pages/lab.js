ac7c36594ab5af47256a9a8bcb5228df755505ac
f-simard <filippa.simard@gmail.com>
2025-03-05 10:44:01 -0500

    add banner and pop-ips

---
+++ assets/js/pages/lab.js
@@ -0,0 +1,22 @@
+import ToastModale from "../composantes/ToastModale.js";
+import Parallax from "../composantes/Parallax.js";
+
+
+(function () {
+	const btnsLaunchToast = document.querySelector(".js-launchToast");
+
+	btnsLaunchToast.addEventListener("click", launchToast);
+
+	function launchToast(event) {
+		const trigger = event.target;
+		const demoCase = trigger.dataset.demo;
+
+		const autoDestruction = demoCase.includes("Auto");
+
+		const msg = "Ceci est une super modale";
+
+		new ToastModale(msg, demoCase, autoDestruction);
+	}
+	
+window.customElements.define("p-arallax", Parallax);
+})();
