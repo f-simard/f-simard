eab8e7d8de84cc7db9e8b8259a1ebec362b0a074
f-simard <filippa.simard@gmail.com>
2025-03-12 15:57:59 -0400

    fix

--- assets/js/pages/lab.js
+++ assets/js/pages/lab.js
@@ -1,5 +1,5 @@
 import ToastModale from "../composantes/ToastModale.js";
-import Parallax from "../composantes/parallax.js";
+import Parallax from "../composantes/Parallax.js";
 import Gallery from "../composantes/Gallery.js";
 
 (function () {
